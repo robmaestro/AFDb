3e3eeadd0d9de45b12caff7d06f8dc776555436a
Ming-04 <jlmingoy@asticom.com.ph>
2022-10-26 16:38:53 +0800

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,29 @@ import React from "react";
 export default function Header() {
     return (
         <>
-            <div></div>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+	  <div class="container-fluid">	    
+		<div class="collapse navbar-collapse " id="navbarSupportedContent">
+		  <div class="mx-auto">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+			<a class="navbar-brand " href="#">AFDB</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">SIGN IN</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">CREATE ACCOUNT</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">FILMS</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">LISTS</a>
+                </li>
+            </ul>
+		</div>
+		</div>
+	  </div>
+	</nav>
         </>
     )
 
