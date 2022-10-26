5d0f5837d3d5aa3e6af6aecc765095ca03186ef6
robmaestro <robbielaza@gmail.com>
2022-10-26 15:46:00 +0800

    Another commit

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Body() {
+    return (
+        <>
+            <div className="main-body">
+                test
+            </div>
+        </>
+    )
+
+}
