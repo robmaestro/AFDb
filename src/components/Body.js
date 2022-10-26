fa21fd6fb1eae38454c4d9ba53cab1751d9f1fbf
robmaestro <robbielaza@gmail.com>
2022-10-26 16:52:15 +0800

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,10 +5,14 @@ export default function Body() {
         <>
             <div className="main-body">
                 <div className="banner-header">
-
+                    <span>Let's see some movies today?</span>
+                    <img className="image-banner" src="https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg"/>
                 </div>
                 <div className="featured-movies">
-                    
+
+                </div>
+                <div className="random-movies">
+
                 </div>
             </div>
         </>
