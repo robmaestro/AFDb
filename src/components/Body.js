f04cab030412cdff5bd8b7899a1ddbae547de308
robmaestro <robbielaza@gmail.com>
2022-10-27 16:33:32 +0800

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,11 +6,11 @@ export default function Body() {
             <div className="main-body">
                 <div className="banner-header">
                     <span className="banner-header-title">What are you watching today?</span>
-                    <img className="image-banner" src="https://api.time.com/wp-content/uploads/2014/11/interstellar.jpg?quality=85&w=3600" alt="banner"/>
+                    <img className="image-banner" src="https://api.time.com/wp-content/uploads/2014/11/interstellar.jpg?quality=85&w=3600" alt="banner" />
                 </div>
                 <div className="featured-movies">
                     <div>
-                        <span>Featured Movies</span>
+                        <span>FEATURED MOVIES</span>
                     </div>
                     <div className="featured-movies-content">
                         <img alt="movie poster" className="featured-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg" />
@@ -22,7 +22,7 @@ export default function Body() {
                 </div>
                 <div className="upcoming-movies">
                     <div>
-                        <span>Upcoming Movies</span>
+                        <span>UPCOMING MOVIES</span>
                     </div>
                     <div className="upcoming-movies-content">
                         <img alt="movie poster" className="upcoming-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg" />
@@ -35,7 +35,14 @@ export default function Body() {
 
                 <div className="browse-movies">
                     <div>
-                        <span>Browse Movies</span>
+                        <span>BROWSE MOVIES</span>
+                    </div>
+                    <div className="browse-movies-content">
+                        <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg" />
+                        <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_.jpg" />
+                        <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BMGUwZjliMTAtNzAxZi00MWNiLWE2NzgtZGUxMGQxZjhhNDRiXkEyXkFqcGdeQXVyNjU1NzU3MzE@._V1_.jpg" />
+                        <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg" />
+                        <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BMzQxNzQzOTQwM15BMl5BanBnXkFtZTgwMDQ2NTcwODM@._V1_.jpg" />
                     </div>
                     <div className="browse-movies-content">
                         <img alt="movie poster" className="browse-movies-content-images" src="https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg" />
@@ -48,9 +55,10 @@ export default function Body() {
 
                 <div className="account-prompt">
                     <div>
-                        <span>Don't have an account? Sign-up it's FREE!</span>
+                        <span>DON'T HAVE AN ACCOUNT? SIGN UP! IT'S FREE</span>
                     </div>
                     <div className="browse-movies-content">
+                        
                     </div>
                 </div>
             </div>
