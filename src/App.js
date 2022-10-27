0863f0299545e67f789bc5c06f90eca8a9b1c94e
Ming-04 <jlmingoy@asticom.com.ph>
2022-10-27 17:51:53 +0800

    Sign In and Sign Up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './css/App.css';
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import { useState } from 'react';
+import CreateAccountModal from './components/CreateAccountModal';
+import SignInModal from './components/SignInModal';
 
 
 function App() {
 
+  
   return (
     <>
-      <Header />
+      <SignInModal />
+      <CreateAccountModal />
+      <Header/>
       <Body /> 
       <Footer />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,34 @@
 import React from "react";
 
 export default function Header() {
+
     return (
         <>
-    <nav className="navbar navbar-expand-sm">
-	  <div className="container-fluid">	    
-		<div className="collapse navbar-collapse " id="navbarSupportedContent">
-		  <div className="mx-auto">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-			<a className="navbar-brand " href="#">
-                <img src="./icon.png" alt="" width="30" height="24" />
-            </a>
-                <li className="nav-item">
-                  <a className="nav-link">SIGN IN</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">CREATE ACCOUNT</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">FILMS</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">LISTS</a>
-                </li>
-            </ul>
-		</div>
-		</div>
-	  </div>
-	</nav>
+            <nav className="navbar navbar-expand-sm">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                        <div className="mx-auto">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <a className="navbar-brand " href="#">
+                                    <img src="./icon.png" alt="" width="30" height="24" />
+                                </a>
+                                <li className="nav-item">
+                                    <a className="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">SIGN IN</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop">CREATE ACCOUNT</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">FILMS</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">LISTS</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 
